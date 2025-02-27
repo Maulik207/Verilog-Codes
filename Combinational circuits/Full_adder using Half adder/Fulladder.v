889bf7c47ddf06fc503860d5cf8018af10c6d75a
Maulik Shah <97025013+Maulik207@users.noreply.github.com>
2025-02-27 11:50:32 +0530

    Create Fulladder.v

---
+++ Combinational circuits/Full_adder using Half adder/Fulladder.v
@@ -0,0 +1,14 @@
+`include "ha.v"
+
+module fa(
+input a,b,c,
+output sum,carry);
+
+wire w1,w2;
+
+ha h1(.a(a),.b(b),.carry(w1),.sum(w2));
+ha h2(.a(w2),.b(c),.carry(w3),.sum(sum));
+
+or(carry,w1,w3);
+
+endmodule
